5f76d9afb87f0acc24e37ee69e6eb438d1b5951b
yhuzued <yusufmait@gmail.com>
2022-11-10 14:00:32 +0700

    Create show controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,30 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
